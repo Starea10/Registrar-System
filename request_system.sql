07de1d6bc8390a7e7ca6344edd49987659f0c46b
Starea10 <70992029+Starea10@users.noreply.github.com>
2026-07-30 07:48:25 +0800

    Add files via upload

--- request_system.sql
+++ request_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2026 at 01:22 PM
+-- Generation Time: Jul 30, 2026 at 01:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,8 +40,17 @@ CREATE TABLE `archive_history` (
   `student_name` varchar(255) DEFAULT NULL,
   `is_archived` tinyint(1) DEFAULT 0,
   `released_at` datetime DEFAULT NULL,
-  `document_path` varchar(255) DEFAULT NULL
+  `document_path` varchar(255) DEFAULT NULL,
+  `email_address` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `archive_history`
+--
+
+INSERT INTO `archive_history` (`id`, `title`, `description`, `claiming_date`, `requester_id`, `status`, `created_at`, `updated_at`, `student_number`, `student_name`, `is_archived`, `released_at`, `document_path`, `email_address`) VALUES
+(6, 'Request for : 1x Transcript of Record (TOR)', 'Student Number: 29987654\nStudent Name: Testing\nProgram: BSCS\nYear of Graduation: \nContact Information: gtabang310@gmail.com\nPurpose: Employment\nRequested Documents: 1x Transcript of Record (TOR)\nScheduled Claiming Date: 2026-08-01', '2026-08-01', 1, 'released', '2026-07-29 14:39:30', '2026-07-29 16:32:51', '29987654', 'Testing', 1, '2026-07-24 23:21:00', NULL, 'gtabang310@gmail.com'),
+(8, 'Request for : 1x Transcript of Record (TOR)', 'Student Number: 29987654\nStudent Name: test\nProgram: BSCS\nYear of Graduation: \nContact Information: 123456789\nPurpose: Employment\nRequested Documents: 1x Transcript of Record (TOR)\nScheduled Claiming Date: 2026-07-31', '2026-07-31', 1, 'released', '2026-07-29 16:38:01', '2026-07-29 17:18:28', '29987654', 'test', 1, '2026-07-30 01:18:28', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -55,6 +64,55 @@ CREATE TABLE `audit_trail` (
   `action` varchar(255) NOT NULL,
   `details` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `audit_trail`
+--
+
+INSERT INTO `audit_trail` (`id`, `user_id`, `action`, `details`, `created_at`) VALUES
+(1, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 14:12:35'),
+(2, 1, 'delete_request', 'Permanently deleted request #1', '2026-07-29 14:12:49'),
+(3, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 14:13:08'),
+(4, 1, 'create_request', 'Created new request: Request for : 1x Diploma', '2026-07-29 14:14:08'),
+(5, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 14:17:33'),
+(6, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 14:17:57'),
+(7, 1, 'delete_request', 'Permanently deleted request #5', '2026-07-29 14:18:42'),
+(8, 1, 'delete_request', 'Permanently deleted request #2', '2026-07-29 14:18:45'),
+(9, 1, 'delete_request', 'Permanently deleted request #3', '2026-07-29 14:18:51'),
+(10, 1, 'delete_request', 'Permanently deleted request #4', '2026-07-29 14:18:54'),
+(11, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 14:39:30'),
+(12, 2, 'create_request', 'Created new request: Request for : 1x Diploma', '2026-07-29 14:45:17'),
+(13, 2, 'delete_request', 'Permanently deleted request #7', '2026-07-29 15:01:21'),
+(14, 1, 'update_request', 'Updated request #6 status from \'pending\' to \'released\' and automatically archived', '2026-07-29 15:21:33'),
+(15, 1, 'update_released_date', 'Updated released date for request #6 from \'2026-07-29\' to \'2026-07-31T23:21\'', '2026-07-29 16:26:13'),
+(16, 1, 'update_released_date', 'Updated released date for request #6 from \'2026-07-31\' to \'2026-07-23T23:21\'', '2026-07-29 16:28:38'),
+(17, 1, 'update_released_date', 'Updated released date for request #6 from \'2026-07-23\' to \'2026-07-24T23:21\'', '2026-07-29 16:32:51'),
+(18, 1, 'create_request', 'Created new request: Request for : 1x Transcript of Record (TOR)', '2026-07-29 16:38:01'),
+(19, 1, 'update_request', 'Updated request #8 status from \'pending\' to \'processing\'', '2026-07-29 16:38:05'),
+(20, 1, 'update_request', 'Updated request #8 status from \'processing\' to \'released\' and automatically archived', '2026-07-29 17:18:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_requests`
+--
+
+CREATE TABLE `pending_requests` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `claiming_date` date DEFAULT NULL,
+  `requester_id` int(11) DEFAULT NULL,
+  `status` enum('submitted') DEFAULT 'submitted',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `student_number` varchar(50) DEFAULT NULL,
+  `student_name` varchar(255) DEFAULT NULL,
+  `is_archived` tinyint(1) DEFAULT 0,
+  `released_at` datetime DEFAULT NULL,
+  `document_path` varchar(255) DEFAULT NULL,
+  `email_address` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -102,7 +160,8 @@ CREATE TABLE `requests` (
   `student_name` varchar(255) DEFAULT NULL,
   `is_archived` tinyint(1) DEFAULT 0,
   `released_at` datetime DEFAULT NULL,
-  `document_path` varchar(255) DEFAULT NULL
+  `document_path` varchar(255) DEFAULT NULL,
+  `email_address` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -113,17 +172,18 @@ CREATE TABLE `requests` (
 
 CREATE TABLE `staffs` (
   `id` int(11) NOT NULL,
-  `staff_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `staff_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `is_deleted` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `staffs`
 --
 
-INSERT INTO `staffs` (`id`, `staff_name`) VALUES
-(1, 'MJ'),
-(2, 'Zyn'),
-(3, 'Pearl');
+INSERT INTO `staffs` (`id`, `staff_name`, `is_deleted`) VALUES
+(1, 'MJ', 0),
+(2, 'Zyn', 0),
+(3, 'Pearl', 0);
 
 -- --------------------------------------------------------
 
@@ -166,6 +226,13 @@ ALTER TABLE `audit_trail`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `pending_requests`
+--
+ALTER TABLE `pending_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `requester_id` (`requester_id`);
+
+--
 -- Indexes for table `requests`
 --
 ALTER TABLE `requests`
@@ -193,19 +260,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `archive_history`
 --
 ALTER TABLE `archive_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `audit_trail`
 --
 ALTER TABLE `audit_trail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `pending_requests`
+--
+ALTER TABLE `pending_requests`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `staffs`
